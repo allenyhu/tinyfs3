84adfe6e4b81186a7d8ed8727e235227f5d05aca
Allen Huang <allenyhu@usc.edu>
2018-04-28 11:34:27 -0700

    RID

--- src/com/client/RID.java
+++ src/com/client/RID.java
@@ -2,4 +2,31 @@ package com.client;
 
 public class RID {
 	//NEEDS TOSTRING METHOD FOR LOGGING
+	private String chunkHandle;
+	private int index; //index for array at end of chunk
+	
+	public RID(String chunkHandle, int index) {
+		this.chunkHandle = chunkHandle;
+		this.index = index;
+	}
+	
+	public RID(String logString) {
+		String[] log = logString.split("|");
+		this.chunkHandle = log[0];
+		this.index = Integer.parseInt(log[1]);
+	}
+	
+	public String getChunkHandle() {
+		return this.chunkHandle;
+	}
+	
+	public int getIndex() {
+		return this.index;
+	}
+	
+	@Override
+	public String toString() {
+		return this.chunkHandle + "|" + Integer.toString(this.index);
+	}
+	
 }
--- src/com/master/LogParser.java
+++ src/com/master/LogParser.java
@@ -41,7 +41,7 @@ public class LogParser {
 							  break;
 					case "D": this.delete(op[1]);
 							  break;
-					case "W": this.write(op[1], op[2], op[3]); //filename, byte[], RID
+					case "W": this.write(op[1], op[2], op[3]); //filename | byte[] | RID
 							  break;
 				}
 			}
