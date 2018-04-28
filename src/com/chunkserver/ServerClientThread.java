9ae1ef4fbdc18ceacaddc62c4bed2663ef5a7b61
Allen Huang <allenyhu@usc.edu>
2018-04-28 16:23:00 -0700

    scthread

--- src/com/chunkserver/ServerClientThread.java
+++ src/com/chunkserver/ServerClientThread.java
@@ -1,8 +1,14 @@
 package com.chunkserver;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+
+import com.client.ClientFS.FSReturnVals;
+import com.messages.FSMessage;
+
+import recordmessages.RecordMessage;
 
 public class ServerClientThread extends Thread {
 	private Socket s;
@@ -21,6 +27,23 @@ public class ServerClientThread extends Thread {
 	
 	@Override
 	public void run() {
-		
+		RecordMessage returnMessage = null;
+		try {
+			while(true){
+				//read message, and get updated message object once the action has executed
+				RecordMessage mess = (RecordMessage) ois.readObject();
+				returnMess = cfsmh.processMessage(mess, master);
+
+				oos.reset();
+				oos.writeObject(returnMess);
+				oos.flush();
+			}
+		} catch (ClassNotFoundException | IOException e) {
+			//System.out.println("client disconnected from master");
+		}finally{
+			if (returnMess.returnVal == FSReturnVals.Success){
+				master.WriteLog(returnMess);
+			}
+		}
 	}
 }
