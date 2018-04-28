51737787299a1fb97363fd434e49e7628766ac9f
Allen Huang <allenyhu@usc.edu>
2018-04-28 15:39:43 -0700

    chunk and RID update

--- src/com/chunkserver/Chunk.java
+++ src/com/chunkserver/Chunk.java
@@ -3,6 +3,7 @@ package com.chunkserver;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 import utility.Constants;
 
@@ -36,6 +37,7 @@ public class Chunk {
 			this.writeHeader();
 		} catch (IOException ioe) {
 			System.out.println("Chunk " + this.chunkHandle + " constructor ioe: " + ioe.getMessage());
+			this.chunkHandle = null;
 		}
 		this.writeHeader();
 	}
@@ -75,7 +77,7 @@ public class Chunk {
 			raf.write(payload, this.freeOffset, payload.length);
 			
 			//update RID array
-			raf.seek(Constants.chunkSize - (4*this.numRecords+1)); //4 bytes back from current entry
+			raf.seek(Constants.chunkSize - (4*(this.numRecords+1))); //4 bytes back from current entry
 			raf.writeInt(this.freeOffset);
 			raf.close();
 			
@@ -107,7 +109,7 @@ public class Chunk {
 	 * @return false not enough space
 	 */
 	public boolean enoughSpace(int size) {
-		return (freeSpace <= size);
+		return (this.freeSpace <= (size+2));
 	}
 	
 	/**
@@ -116,7 +118,9 @@ public class Chunk {
 	 * @return payload 	bytes of record
 	 * @return null		error with reading
 	 */
-	public byte[] getRecord(int index) { 
+	// if arraylist is size 1, assume record
+	// if arraylist is > 1, assume metarecord
+	public ArrayList<byte[]> getRecord(int index) { 
 		try {
 			RandomAccessFile raf = new RandomAccessFile(this.filePath + "/" + this.chunkHandle, "rw");
 			int RIDoffset = Constants.chunkSize - (4*(index+1)); //index of RID in chunk file
@@ -125,30 +129,62 @@ public class Chunk {
 			
 			int size = -1;
 			if(index == (this.numRecords-1)) { //last record 
-				size = offset - this.freeOffset; //goes upto freeOffset
+				size = offset - this.freeOffset - 2; //goes upto freeOffset
 			} else { //use next record's offset to calculate this record's size size
 				raf.seek(RIDoffset-4);
-				size = offset - raf.readInt();
+				size = offset - raf.readInt() - 2;
 			}
 			
+			ArrayList<byte[]> payloadArrayList = new ArrayList<byte[]>();
 			byte[] payload = new byte[size]; //byte[] to be read to
 			int counter = 0;
 			
-			raf.seek(offset);
-			while(counter < size) { //insure read correct # of bytes
-				counter = raf.read(payload, counter, size);
-				if(counter == -1) {
-					raf.close();
-					return null; //reached end of file incorrectly
+			//grabbing payload
+			raf.seek(offset); 
+			if(raf.readByte() == (byte)1) { //is metadata
+				//parse 12 byte sections: chunkhandle + RID index
+				raf.seek(offset+2);
+				
+				//ASSUME SIZE MULTIPLE OF 12
+				if(size%12 != 0) {
+					System.out.println("CHUNK GET RECORD ASSUMPTION WRONG");
+					System.out.println("Size is " + size + " Offset is " + offset);
 				}
+				
+				for(int i=0; i < size; i+=12) {
+					byte[] temp = new byte[12];
+					int cnt = 0;
+					while(cnt < 12) { //read 12 bytes
+						cnt = raf.read(temp, cnt, 12);
+						if(cnt == -1) {
+							raf.close();
+							return null;
+						}
+					}
+					payloadArrayList.add(temp);
+				}
+				
+			} else {
+				raf.seek(offset+2); //advance past sub/not sub record byte
+				while(counter < size) { //insure read correct # of bytes
+					counter = raf.read(payload, counter, size);
+					if(counter == -1) {
+						raf.close();
+						return null; //reached end of file incorrectly
+					}
+				}
+				payloadArrayList.add(payload);
 			}
+			
 			raf.close();
-			return payload;
+			return payloadArrayList;
 		} catch (IOException ioe) {
 			System.out.println(this.chunkHandle + " getRecord() ioe: " + ioe.getMessage());
 		}
 		return null;
 	}
 	
-	
+	public String getChunkHandle() {
+		return this.chunkHandle;
+	}
 }
