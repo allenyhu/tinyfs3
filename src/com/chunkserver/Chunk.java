a6c83f35103f86f35e2dddfb661ba1a55cf4fcd6
Allen Huang <allenyhu@usc.edu>
2018-04-28 13:15:49 -0700

    chunk helper functions

--- src/com/chunkserver/Chunk.java
+++ src/com/chunkserver/Chunk.java
@@ -2,31 +2,71 @@ package com.chunkserver;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 
 import utility.Constants;
 
 public class Chunk {
+	private String filePath;
 	private byte[] bytes; //actual bytes in chunk
 	private int numRecords;
 	private int freeSpace; //# bytes available; this will always be 4 bytes less than actual free bytes to store record's offset
 	private int freeOffset; //offset of free space from front of byte[]
 	private String chunkHandle;
 	
-	public Chunk(String chunkHandle) {
+	public Chunk(String filePath, String chunkHandle) {
+		this.filePath = filePath;
 		this.chunkHandle = chunkHandle;
 		numRecords = 0;
 		freeSpace = Constants.chunkSize - 16; //12 for numRecords, freeSpace, freeOffset; 4 for RID offset
 		freeOffset = 12;
+		
+		this.writeHeader();
+	}
+	
+	private void writeHeader() {
 		try {
-			RandomAccessFile raf = new RandomAccessFile(chunkHandle, "rw");
+			//Byte array for numRecords, freeSpace, freeOffset
+			ByteBuffer bb = ByteBuffer.allocate(12);
+			bb.putInt(0, this.numRecords);
+			bb.putInt(4, this.freeSpace);
+			bb.putInt(8, this.freeOffset);
+			
+			//write to beginning of RAF
+			RandomAccessFile raf = new RandomAccessFile(this.filePath + "/" + this.chunkHandle, "rw");
 			raf.seek(0);
-			raf.writeInt(numRecords);
-			raf.writeInt(freeSpace);
-			raf.writeInt(freeOffset);
+			raf.write(bb.array());
 			raf.close();
 		} catch (IOException ioe) {
-			System.out.println("Chunk " + chunkHandle + " constructor ioe: " + ioe.getMessage());
+			System.out.println("Chunk " + this.chunkHandle + " writeHeader ioe: " + ioe.getMessage());
 		}
+	}
+	
+	public int writeRecord(byte[] payload) {
+		try {
+			//If the file corresponding to ChunkHandle does not exist then create it before writing into it
+			RandomAccessFile raf = new RandomAccessFile(this.filePath + "/" + this.chunkHandle, "rw");
+			raf.write(payload, this.freeOffset, payload.length);
+			raf.close();
+			
+			this.updateHeader(payload.length);
+			return (this.numRecords - 1);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
+	}
+	
+	/**
+	 * Helper function for writeRecord
+	 * @param size # bytes in payload
+	 */
+	private void updateHeader(int size) {
+		this.numRecords++;
+		this.freeSpace -= (size + 4); //4 for the additional RID entry
+		this.freeOffset += size;
+		
+		this.writeHeader();
 	}
 	
 	public boolean enoughSpace(int size) {
--- src/com/client/ClientFS.java
+++ src/com/client/ClientFS.java
@@ -1,5 +1,10 @@
 package com.client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ClientFS {
 	
 	private Socket s;
